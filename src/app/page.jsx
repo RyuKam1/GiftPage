4c379772cf6c2726a68c37ed9cddd3d9a35124ed
RyuKam1 <omarashvili.giorgi07@gmail.com>
2024-05-27 13:45:27 +0400

    reworked link generation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./page.module.css";
 
 const links = [
   "https://www.youtube.com/watch?v=q-Y0bnx6Ndw",
-  "https://www.youtube.com/watch?v=5w1W1t_3WBo",
+  "https://www.youtube.com/watch?v=5w1t_3WBo",
   "https://www.youtube.com/shorts/38Nn50S5kWg",
   "https://www.youtube.com/watch?v=De6rxtML3JI",
 ];
 
-function page() {
+function Page() {
+  const [randomLink, setRandomLink] = useState(
+    links[Math.floor(Math.random() * links.length)]
+  );
+
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * links.length);
+    window.location.href = links[randomIndex];
+  };
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.main}>
@@ -17,9 +28,12 @@ function page() {
           <img src="right.png" alt="arrow" className={styles.arrow} />
         </div>
         <div className={styles.imgContainer}>
-          <a href={`${links[Math.floor(Math.random() * links.length)]}`}>
-            <img src="giftBox.png" alt="gift" className={styles.image} />
-          </a>
+          <img
+            src="giftBox.png"
+            alt="gift"
+            className={styles.image}
+            onClick={handleClick}
+          />
         </div>
         <div className={styles.pressRight}>
           <img src="left.png" alt="arrow" className={styles.arrow} />
@@ -30,4 +44,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
